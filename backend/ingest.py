f46a50c7b26626b57d1fe5284da5f75d70395ecc
Subata khan <subatakhan2006@gmail.com>
2026-09-05 15:29:13 +0500

    chore: migrate to langchain huggingface embeddings

--- backend/ingest.py
+++ backend/ingest.py
@@ -4,7 +4,7 @@ from langchain_core.documents import Document
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from dotenv import load_dotenv
 
 load_dotenv()
